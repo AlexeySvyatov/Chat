2c244722f5d399a338994402c4763cc99283f0f7
Alexey Svyatov <svyatovalexe1@yandex.ru>
2023-05-10 16:28:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chat/models/ChatMessage.java
@@ -0,0 +1,10 @@
+package com.example.chat.models;
+
+import java.util.Date;
+
+public class ChatMessage {
+    public String messageId, senderId, receiverId, message, dateTime;
+    public Date dateObject;
+    public String conversionId, conversionName, conversionImage;
+    public int unreadMessages;
+}
